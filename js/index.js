e5e0b6e85a4951647cebfb76f4b9aa0fcadbfb64
cachu <gcachu.o@gmail.com>
2017-12-11 13:16:40 -0600

    oscar

---
+++ js/index.js
@@ -0,0 +1,70 @@
+var animation4 = bodymovin.loadAnimation({
+  container: document.getElementById('bm'),
+  renderer: 'svg',
+  loop: true,
+  autoplay: true,
+  path: 'js/monitas.json'
+});
+var animation2 = bodymovin.loadAnimation({
+  container: document.getElementById('bm2'),
+  renderer: 'svg',
+  loop: true,
+  autoplay: true,
+  path: 'js/data-f2.json'
+});
+var animation3 = bodymovin.loadAnimation({
+  container: document.getElementById('bm3'),
+  renderer: 'svg',
+  loop: true,
+  autoplay: true,
+  path: 'js/data-f1.json'
+});
+var animation = bodymovin.loadAnimation({
+  container: document.getElementById('bm4'),
+  renderer: 'svg',
+  loop: true,
+  autoplay: true,
+  path: 'js/lampara.json'
+});
+var animation = bodymovin.loadAnimation({
+  container: document.getElementById('bm5'),
+  renderer: 'svg',
+  loop: true,
+  autoplay: true,
+  path: 'js/estudiantes-azul.json'
+});
+var animation = bodymovin.loadAnimation({
+  container: document.getElementById('es1'),
+  renderer: 'svg',
+  loop: true,
+  autoplay: true,
+  path: '../js/estudiantes2.json'
+});
+var animation = bodymovin.loadAnimation({
+  container: document.getElementById('es3'),
+  renderer: 'svg',
+  loop: true,
+  autoplay: true,
+  path: 'js/estudiantes2.json'
+});
+var animation = bodymovin.loadAnimation({
+  container: document.getElementById('es2'),
+  renderer: 'svg',
+  loop: true,
+  autoplay: true,
+  path: '../js/estudiantes-ch.json'
+});
+var animation = bodymovin.loadAnimation({
+  container: document.getElementById('f-m'),
+  renderer: 'svg',
+  loop: true,
+  autoplay: true,
+  path: 'js/full-morado.json'
+});
+var animation = bodymovin.loadAnimation({
+  container: document.getElementById('f-a'),
+  renderer: 'svg',
+  loop: true,
+  autoplay: true,
+  path: 'js/full-azul.json'
+});
---
+++ js/parallaxpersp.js
@@ -0,0 +1,1 @@
+$('.parallaxpersp').parallax();
