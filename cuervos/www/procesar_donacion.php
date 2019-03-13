6ef4a26c5a5a92c471677a22d92b9dc845d67f51
Memo Cachú <gcachu.o@gmail.com>
2019-03-13 00:56:20 -0600

    banwire setup

--- cuervos/www/procesar_donacion.php
+++ cuervos/www/procesar_donacion.php
@@ -66,6 +66,94 @@ if (empty($datos)) {
                     <input type="hidden" name="src" value="1">
                     <input type="hidden" name="sra" value="1">
                 </form>
+            <?php else:?>
+            <input type="image" src="../../img/btn-donar-2.png" border="0" name="submit"
+                   id="donar-Normal" onclick="pagar()">
+
+                <script src="js/index.js"></script>
+                <script src="../../banwire/checkout.js"></script>
+                <script>
+                    var SW = new BwGateway({
+                        // Nombre de usuario de Banwire
+                        user: 'habesha',
+                        // Titulo de la entana
+                        title: "Proyecto Habesha",
+                        // Referencia
+                        reference: 'donacion',
+                        // Concepto
+                        concept: 'Donacion',
+                        infoMsg: 'Mensaje personalizado para el cliente',
+                        // Moneda
+                        currency: 'MXN',
+                        // Customer information
+                        cust: {
+                            fname: "",
+                            mname: "",
+                            lname: "",
+                            email: "",
+                            phone: "",
+                            addr: "",
+                            city: "",
+                            state: "",
+                            country: "",
+                            zip: ""
+                        },
+                        ship: {
+                            addr: "",
+                            city: "",
+                            state: "",
+                            country: "",
+                            zip: ""
+                        },
+                        notifyUrl: "http://proyectohabesha.org/banwire/notify.php",
+                        // Opciones de pago
+                        paymentOptions: 'visa,mastercard,amex,spei', // visa,mastercard,amex,oxxo
+                        // Mostrar o no pagina de resumen de compra
+                        reviewOrder: true,
+                        // Handler en caso de exito en el pago
+                        successPage: '',
+                        onSuccess: function (data) {
+                            console.log("Pago correcto");
+                            console.log(data);
+                        },
+                        // Pago pendiente OXXO
+                        pendingPage: '',
+                        onPending: function (data) {
+                            console.log(data);
+                            alert("El pago esta pendiente por ser efectuado");
+                        },
+                        // Pago challenge
+                        challengePage: '',
+                        onChallenge: function () {
+                            alert("Pago enviado a validaciones de seguridad");
+                        },
+                        // Handler en caso de error en el pago
+                        errorPage: '',
+                        onError: function (error) {
+                            console.log(error);
+                        },
+                        // Cuando cierra el popup sin completar el proceso
+                        onCancel: function () {
+                            console.log("Se cancelo el proceso");
+                        }
+                    });
+                    function pagar() {
+                        var total = '<?= $datos['cantidad'] ?>';
+                        SW.pay({
+                            // Total de la compra
+                            total: total,
+                            // Arreglo con los items de compra
+                            items: [
+                                {
+                                    name: "Donación",
+                                    qty: 1,
+                                    desc: "Donación de " + total,
+                                    unitPrice: total
+                                }
+                            ]
+                        });
+                    }
+                </script>
             <?php endif; ?>
         </div>
     </div>
