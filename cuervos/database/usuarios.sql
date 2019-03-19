f0c1b0449656e28e649166ad448f7717d65f1617
Memo Cachú <gcachu.o@gmail.com>
2019-03-19 17:42:01 -0600

    usuarios

---
+++ cuervos/database/usuarios.sql
@@ -0,0 +1,8 @@
+create table if not exists usuarios
+(
+  id       bigint auto_increment primary key,
+  usuario  varchar(100)     not null,
+  password varchar(255)     not null,
+  estatus  bit default b'1' not null,
+  constraint usuarios_usuario_uindex unique (usuario)
+);
