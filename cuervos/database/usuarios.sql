b33af3bcf5e17b964b3bdb6ae587c69edc604b69
Memo Cachú <gcachu.o@gmail.com>
2019-03-19 17:42:45 -0600

    usuarios

--- cuervos/database/usuarios.sql
+++ cuervos/database/usuarios.sql
@@ -5,4 +5,6 @@ create table if not exists usuarios
   password varchar(255)     not null,
   estatus  bit default b'1' not null,
   constraint usuarios_usuario_uindex unique (usuario)
-);
+)
+  engine = InnoDB
+  collate = utf8mb4_general_ci;
