775f0fd7d911684952878558a963a0f09557df28
Memo Cachú <gcachu.o@gmail.com>
2019-03-25 14:36:31 -0600

    descarga

---
+++ cuervos/www/test/api/procesarDonacion.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: memo
+ * Date: 12/03/19
+ * Time: 11:51 PM
+ */
+
+include "mysql.php";
+
+setcookie('XDEBUG_SESSION', 'PHPSTORM');
+ini_set('display_errors', 1);
+error_reporting(E_ALL ^ (E_NOTICE | E_WARNING));
+
+function obtenerDatos($id_donacion)
+{
+    try {
+        $sql = <<<sql
+select *  from donaciones
+inner join donantes on id_donante=donantes.id
+where donaciones.id=$id_donacion and donaciones.estatus='CREADA'
+sql;
+
+        $mysql = new mysql();
+        return $mysql->single_row($mysql->query($sql));
+    } catch (DatabaseException $exception) {
+
+    }
+}
+
+function completaDonacion($id_donacion, &$folios)
+{
+    try {
+        $mysql = new mysql();
+
+        $sql = <<<sql
+select count(1) count from donaciones where id=$id_donacion and estatus='CREADA';
+sql;
+
+        $count = (int)$mysql->single_row($mysql->query($sql), 'count');
+
+        if (!$count) {
+            return false;
+        }
+
+        $sql = <<<sql
+update donaciones set estatus='COMPLETA' where id=$id_donacion and estatus='CREADA';
+sql;
+
+        $mysql->query($sql);
+
+        $folios = $mysql->all_rows($mysql->query(<<<sql
+select id from folios where id_donacion='$id_donacion'
+sql
+        ));
+
+        return true;
+    } catch (DatabaseException $exception) {
+        return false;
+    }
+}
