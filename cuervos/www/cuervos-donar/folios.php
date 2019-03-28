f784e4f02ae1f37b010eed327e79967df46fdd63
Memo Cachú <gcachu.o@gmail.com>
2019-03-27 19:05:26 -0600

    https

--- cuervos/www/cuervos-donar/folios.php
+++ cuervos/www/cuervos-donar/folios.php
@@ -1,28 +1,32 @@
 <head>
     <meta charset="UTF-8">
-        <meta name="viewport" content="width=690">
+    <meta name="viewport" content="width=690">
     <link rel="stylesheet" href="css/index.css">
     <link rel="stylesheet" href="css/donar.css">
     <title>Cuervos Por Habesha</title>
-    <link href="https://fonts.googleapis.com/css?family=Encode+Sans+Semi+Condensed:400,700,900|Encode+Sans:400,700,900" rel="stylesheet">
-    
+    <link href="https://fonts.googleapis.com/css?family=Encode+Sans+Semi+Condensed:400,700,900|Encode+Sans:400,700,900"
+          rel="stylesheet">
+
     <style>
-	.logo-1{
-		position: absolute;
-	}
-		body{
-			color: #fff;
-			font-family: 'Encode Sans', sans-serif;
-		}
-		.card{
-			margin-top: 120px;
-		}
-		.tx-gde{
-			font-size: 1.4em;
-			color: #fff;
-			font-weight: 700;
-		}
-	</style>
+        .logo-1 {
+            position: absolute;
+        }
+
+        body {
+            color: #fff;
+            font-family: 'Encode Sans', sans-serif;
+        }
+
+        .card {
+            margin-top: 120px;
+        }
+
+        .tx-gde {
+            font-size: 1.4em;
+            color: #fff;
+            font-weight: 700;
+        }
+    </style>
 </head>
 
 <body>
@@ -32,7 +36,7 @@
 
 <div class="container pt-3 folios">
     <div class="card">
-       
+
         <div class="card-body" id="login" style="display: none">
             <form class="form-horizontal" onsubmit="return false;">
                 <div class="form-group">
@@ -64,9 +68,9 @@
 inner join donantes d2 on d.id_donante = d2.id order by folios.id") as $folio):
                     ?>
                     <tr class="tx-gde">
-                        <td><?= $folio['nombre'] . $folio['apellidos'] ?></td>
-                        <td><?=$folio['correo']." | ".$folio['telefono']?></td>
-                        <td><?="$folio[metodo] $$folio[cantidad] $folio[tipo]"?></td>
+                        <td><?= $folio['nombre'] . " " . $folio['apellidos'] ?></td>
+                        <td><?= $folio['correo'] . " | " . $folio['telefono'] ?></td>
+                        <td><?= "$folio[metodo] $$folio[cantidad] $folio[tipo]" ?></td>
                         <td>#CuervoHabesha<?= $folio['id'] ?></td>
                     </tr>
                 <?php endforeach; ?>
